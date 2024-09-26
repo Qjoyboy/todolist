fb7a367f103ac18a60265b831b5e951d2a33ead3
Qjoyboy <khanmagomedovruslan77@gmail.com>
2024-09-26 23:51:57 +0300

    developing todolist

---
+++ pythonProject/todolist/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Quest(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    pub_date = models.DateTimeField("date published")
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+
+    def __str__(self):
+        return self.title
