36d603c16460e9d46d11a88a80736f415cc84e00
Qjoyboy <khanmagomedovruslan77@gmail.com>
2024-09-27 20:09:49 +0300

    added css styles, updating views from fbc to cbv

--- pythonProject/todolist/models.py
+++ pythonProject/todolist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Quest(models.Model):
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'Задание'
--- pythonProject/todolist/views.py
+++ pythonProject/todolist/views.py
@@ -1,4 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import CreateView, DetailView
 
-def index(request):
-    return HttpResponse("Am i working?")
+from todolist.models import Quest
+
+
+class IndexView(generic.ListView):
+    template_name = 'todolist/index.html'
+    context_object_name = 'latest_quest_list'
+
+    def get_queryset(self):
+        return Quest.objects.order_by("-pub_date")[:5]
+
+class DetailView(DetailView):
+    model = Quest
+    template_name = 'todolist/quests.html'
+
+class QuestCreateView(CreateView):
+    model = Quest
+    fields = ('title','description')
+    success_url = reverse_lazy('todolist:index')
+
